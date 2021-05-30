64f78ec9a397c7a03164251957f68d695451c40c
Aaron <aaron.wedral@gmail.com>
2021-05-30 23:40:46 +0200

    Try adept

--- __init__.py
+++ __init__.py
@@ -7,12 +7,14 @@ class CookingSkill(ChatterboxSkill):
         super().__init__()
         self.started_cooking = False
 
-    def initialize(self):
-        my_setting = self.settings.get('my_setting')
-
-
+    """
     @intent_handler('start.cooking.intent')
     def handle_start_cooking_intent(self, message):
+        self.speak_dialog("start.cooking")
+        self.started_cooking = True
+    """
+    @intent_handler(IntentBuilder('Cooking').require('Cooking'))
+    def handle_start_cooking_simple(self, message):
         self.speak_dialog("start.cooking")
         self.started_cooking = True
 
